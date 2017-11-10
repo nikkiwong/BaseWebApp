28d5b7308c691b47afb98ee8a61560fc03a38a09
Nik W <Nik W>
2017-11-10 16:32:26 +0000

    adding add message button to send message to database

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,49 @@
+// This function sets up a listener- '.on()' gets called automatically whenever something saved in '/stream/' changes.
+// It's main purpose is to iterate over the stream in the database and add each message to the page.
+function initializeStreamListener() {
+  const databaseStreamReference = firebase.database().ref('/stream/');
 
-function myFunction() {
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+  databaseStreamReference.on('value', function(snapshot) {
+    var messages = snapshot.val();
+    $('#stream').empty();
 
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
-
-  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="/images/wowdoge.jpeg" />`);
+    if (messages) {
+      Object.keys(messages).forEach(function (key) {
+        const message = messages[key];
+        $('#stream').append(`<div>${message.body}</div>`);
+      });
+    }
+  });
 }
+
+// This function gets called with the new message information.
+// It gets the user information and uses both to add the post to the database.
+function addMessage(body, title) {
+  var user = firebase.auth().currentUser;
+  var authorPic = user.photoURL;
+  var author = user.displayName;
+
+  var postData = {
+    author: author,
+    authorPic: authorPic,
+    title: title,
+    body: body
+  };
+
+  var newPostKey = firebase.database().ref().child('stream').push().key;
+  firebase.database().ref('/stream/' + newPostKey).set(postData);
+}
+
+
+// This gets called whenver the form is submitted (check out the index.ejs).
+// Uses jQuery to get the message info and passes it to 'addMessage to actually submit the info to firebase.
+function handleMessageFormSubmit() {
+  var body = $('#new-post-body').val();
+  var title = $('#new-post-title').val();
+
+  addMessage(body, title);
+}
+
 
 // Gets called whenever the user clicks "sign in" or "sign out".
 function toggleSignIn() {
@@ -16,13 +52,13 @@ function toggleSignIn() {
     provider.addScope('https://www.googleapis.com/auth/plus.login');
     firebase.auth().signInWithPopup(provider).then(function(result) {
       console.log("success");
-      document.getElementById("sign").textContent = "Sign Out";
+      document.getElementById("sign").innerHTML = "Sign Out";
     }).catch(function(error) {
       console.error("error", error);
     });
   } else { // handle logout
     firebase.auth().signOut();
-    document.getElementById("sign").textContent = "Sign In";
+    document.getElementById("sign").innerHTML = "Sign In";
   }
 
   //This disables the button until login or logout is successful
